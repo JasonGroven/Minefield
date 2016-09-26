09a7e7df36ac7cae5d29d1f5da7fa12e785c230d
JasonGroven <jgroven@mail.csuchico.edu>
2016-09-26 11:42:57 -0700

    Added two tests to FieldTest.cpp, and bug descriptions to buglog.md

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -8,17 +8,34 @@
  
 class FieldTest : public ::testing::Test
 {
-	protected:
-		FieldTest(){}
-		virtual ~FieldTest(){}
-		virtual void SetUp(){}
-		virtual void TearDown(){}
+    protected:
+        FieldTest(){}
+        virtual ~FieldTest(){}
+        virtual void SetUp(){}
+        virtual void TearDown(){}
 };
 
 TEST(FieldTest, placeMineInBounds)
 {
-	Field minefield;
-	
-	minefield.placeMine(4,5);
-	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+    Field minefield(10);
+    
+    minefield.placeMine(4,5);
+    ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+TEST(FieldTest, PlacedMineIsNotSafe)
+{
+    Field mf;
+    mf.placeMine(2, 3);
+    ASSERT_FALSE(mf.isSafe(2,3));
+}
+TEST(FieldTest, EmptySquareSafe)
+{
+    Field mf(5);
+    ASSERT_TRUE(mf.isSafe(2,3));
+}
+TEST(FieldTest, placeMineOOB)
+{
+    Field mf(1);
+    mf.placeMine(25, 25);
+    ASSERT_NE( mf.get(25,25), MINE_HIDDEN);
 }
--- buglog.md
+++ buglog.md
@@ -1,1 +1,5 @@
 # BUG LOG
+Placing mines out of boundaries appears to work just fine, implying
+an error in the private function "inBounds(int, int)".
+
+
